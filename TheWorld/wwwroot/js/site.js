b6d63591706ebdeb6b4190d7d09c923759d6ce7d
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-25 10:03:33 -0800

    Updated view and javascript.

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// site.js
+
+function startup() {
+    var ele = document.getElementById("username");
+
+    ele.innerHTML = "Paul Lewallen";
+
+    var main = document.getElementById("main");
+
+    main.onmouseenter = function () {
+        main.style.backgroundColor = "#888";
+    };
+
+    main.onmouseleave = function () {
+        main.style.backgroundColor = "";
+    }
+}
+
+startup();
