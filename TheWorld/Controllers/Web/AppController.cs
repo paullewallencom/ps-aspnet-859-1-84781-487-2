086dc320cfe0ea7374f1aa15f34bd91d8306ee87
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-25 15:53:24 -0800

    Added constructor to Controller to test Service.

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -4,12 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheWorld.Services;
 using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -24,6 +31,8 @@ namespace TheWorld.Controllers.Web
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
+            _mailService.SendMail("paul@aol.com", model.Email, "From TheWorld", model.Message);
+
             return View();
         }
 
