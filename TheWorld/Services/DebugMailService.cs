ab75dca03f1b9d1ca3ccbd827ff840b16d16ee2c
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-25 15:48:25 -0800

    Adding stub to check service.

---
+++ TheWorld/Services/DebugMailService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.Services
+{
+    public class DebugMailService : IMailService
+    {
+        public void SendMail(string to, string from, string subject, string body)
+        {
+            //throw new NotImplementedException();
+
+            Debug.WriteLine($"Sending Mail: To: {to} From: {from} Subject: {subject}");
+        }
+    }
+}
--- TheWorld/Services/IMailService.cs
+++ TheWorld/Services/IMailService.cs
@@ -8,6 +8,6 @@ namespace TheWorld.Services
 {
     public interface IMailService
     {
-        
+        void SendMail(string to, string from, string subject, string body);
     }
 }
