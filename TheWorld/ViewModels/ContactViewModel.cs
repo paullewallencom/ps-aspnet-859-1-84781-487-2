4832a23072561c362e0ea1d20f63febb56b4717a
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-25 14:47:56 -0800

    Added Model validation with attributes.

--- TheWorld/ViewModels/ContactViewModel.cs
+++ TheWorld/ViewModels/ContactViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace TheWorld.ViewModels
 {
     public class ContactViewModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [StringLength(4096, MinimumLength = 10)]
         public string Message { get; set; }
 
     }
