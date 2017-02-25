64a9cdb94665bbcb9b583c269d55df8ac211edda
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-25 11:43:39 -0800

    Added Controllers for the Views.

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -13,5 +13,15 @@ namespace TheWorld.Controllers.Web
         {
             return View();
         }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
