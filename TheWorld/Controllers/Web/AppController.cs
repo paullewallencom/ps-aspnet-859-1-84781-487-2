2e111cbb86c65f82cc7fdffb2227e68a7d56c581
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-25 11:09:15 -0800

    Updated dependency for MVC.

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace TheWorld.Controllers.Web
 {
-    public class AppController
+    public class AppController : Controller
     {
         
     }
