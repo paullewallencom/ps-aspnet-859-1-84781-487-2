294a10ef781186044eacd0ec17f31efeaa18d99e
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-25 15:56:17 -0800

    Configured custom service for startup.

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TheWorld.Services;
 
 namespace TheWorld
 {
@@ -16,6 +17,8 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMailService, DebugMailService>();
+
             services.AddMvc();
         }
 
