3e020ea5af91a8bd9d8e7be9d001ba3859d186c7
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-25 14:37:31 -0800

    Added fields to ContactViewModel.

--- TheWorld/ViewModels/ContactViewModel.cs
+++ TheWorld/ViewModels/ContactViewModel.cs
@@ -8,6 +8,9 @@ namespace TheWorld.ViewModels
 {
     public class ContactViewModel
     {
-        
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+
     }
 }
