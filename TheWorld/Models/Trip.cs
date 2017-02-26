b23ed9929f4a561f7587c3c3035546ca1acdd2c4
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-26 10:44:48 -0800

    Adding a Collection of Stops to the Trip Entity.

--- TheWorld/Models/Trip.cs
+++ TheWorld/Models/Trip.cs
@@ -12,5 +12,7 @@ namespace TheWorld.Models
         public string Name { get; set; }
         public DateTime DateCreated { get; set; }
         public string UserName { get; set; }
+
+        public ICollection<Stop> Stops { get; set; }
     }
 }
