84f638a6c713e327ffa3d5c2579cbea6a4e53160
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-25 20:47:34 -0800

    Environment variable: MailSettings_ToAddress taco@aol.com

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -23,7 +23,8 @@ namespace TheWorld
 
             var builder = new ConfigurationBuilder()
                 .SetBasePath(_env.ContentRootPath)
-                .AddJsonFile("config.json");
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
 
             _config = builder.Build();
 
